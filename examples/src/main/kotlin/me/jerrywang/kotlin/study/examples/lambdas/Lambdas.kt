7633eaa1ce6e4062d44380fdc927660abbcdf754
Jerry Wang <j3rry.wan9@gmail.com>
2019-05-25 11:20:37 -0700

    add an example for lambda expressions

---
+++ examples/src/main/kotlin/me/jerrywang/kotlin/study/examples/lambdas/Lambdas.kt
@@ -0,0 +1,34 @@
+package me.jerrywang.kotlin.study.examples.lambdas
+
+typealias DoubleConversion = (Double) -> Double
+
+fun convert(x: Double, converter: DoubleConversion): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun  getConversionLambda(str: String): DoubleConversion {
+    when (str) {
+        "CentigradeToFahrenheit" -> return { it * 1.8 + 32 }
+        "KgsToPounds" -> return { it * 2.204623 }
+        "PoundsToUSTons" -> return { it / 2000.0 }
+        else -> return { it }
+    }
+}
+
+fun combine(lambda1: DoubleConversion, lambda2: DoubleConversion): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
+}
+
+fun main() {
+    println("Convert 2.5 kg to pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
+}
